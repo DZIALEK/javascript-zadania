a8865440f6e2783fb5f59fcbcbb4bb4ec4333f1a
DZIALEK <62839554+DZIALEK@users.noreply.github.com>
2021-02-05 09:03:12 +0100

    Update script.js
    add zad3

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -33,3 +33,68 @@ array.forEach(a => {
 array.forEach(b => {
   if (typeof b === "number") console.log(b);
 })
+
+
+
+// zad.3
+
+const exampleArray = [1, 2, 3, 4, 5, 6, 7];
+
+// 1)
+const sum = exampleArray.reduce((sum, element) => (sum += element));
+console.log(`\nsuma wszystkich elementów: ${sum}`);
+
+// 2)
+const dif = exampleArray.reduce((dif, element) => (dif -= element));
+console.log(`\nróżnica wszystkich elementów: ${dif}`);
+
+// 3)
+const average = sum / exampleArray.length;
+console.log(`\nśrednia wszystkich elementów: ${average}`);
+
+// 4)
+console.log("\nparzyste elementy: ");
+exampleArray.forEach((element) => {
+  if (element % 2 === 0) {
+    console.log("parzyste: ", element);
+  }
+});
+
+// 5)
+console.log("\nelementy nieparzyste: ");
+exampleArray.forEach((element) => {
+  if (element % 2 !== 0) {
+    console.log("nieparzysta: ", element);
+  }
+});
+
+// 6)
+let max = exampleArray[0];
+exampleArray.forEach((element) => {
+  if (element > max) {
+    max = element;
+  }
+});
+
+console.log(`\nliczba największa : ${max}`);
+
+// 7)
+let min = exampleArray[0];
+exampleArray.forEach((element) => {
+  if (element < min) {
+    min = element;
+  }
+});
+
+
+// 8)
+console.log("\nwypisz tablicę od tyłu:");
+
+for (let i = exampleArray.length - 1; i >= 0; i--) {
+  console.log(exampleArray[i]);
+}
+
+
+
+// zad.4
+
