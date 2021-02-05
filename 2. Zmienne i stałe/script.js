61097c3ef7e538e4a5b043cac33bedce740ca450
DZIALEK <62839554+DZIALEK@users.noreply.github.com>
2021-02-05 06:58:31 +0100

    Update script.js
    all done

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,30 @@
+// zad.1
+
+var Number = 12;
+var String = "przykladowy tekst";
+var NumberString = 2 + " przykladowy tekst";
+var Bool = true;
+var Null = null;
+
+console.log("Number: ", Number);
+console.log("String: ", String);
+console.log("NumberString: ", NumberString);
+console.log("Bool: ", Bool);
+console.log("Null: ", Null);
+
+
+
+// zad.2
+const number1 = 2;
+const number2 = 5;
+
+let sum = 0;
+sum = number1 + number2;
+
+console.log(sum);
+
+
+
+//zad.3
+let zmienna1;
+console.log(zmienna1); //let is not defined - zmienna jest utworzona, ale nie przypisano jej wartości
