6110a56809a66142590c53e84997ce0451a9ec87
DZIALEK <62839554+DZIALEK@users.noreply.github.com>
2021-02-05 07:07:19 +0100

    Update script.js
    all task done

--- 3.Operatory/script.js
+++ 3.Operatory/script.js
@@ -1,1 +1,46 @@
-// test 
+// Zadanie 1
+
+var v1 = true;
+var v2 = false;
+console.log(v1 == v2);
+
+
+// Zadanie 2
+var n1 = 2;
+var n2 = 4;
+var moduloResult = 0;
+console.log(moduloResult = n2 % n1);
+
+
+// Zadanie 3
+let word1 = "Pies";
+let word2 = "Kot";
+let stringResult = "";
+console.log(stringResult = word1 + word2);
+
+
+// Zadanie 4:
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString);
+// Zwrócono wynik "true" porównujemy tylko wartości, które są takie same.
+
+console.log(someNumber === someString);
+// Zwrócono wynik "false" porównujemy wartości i typ. Typ się różni stąd wynik false.
+
+
+// Zadanie 5:
+var counter = 30;
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+
+// Zadanie 6:
+let number1 = 1;
+let number2 = 2;
+let result = null;
+result = number1 > number2;
+console.log(result);
