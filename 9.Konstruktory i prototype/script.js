1ebe8f5e4418e5dc0439251fa832ac310e314282
DZIALEK <62839554+DZIALEK@users.noreply.github.com>
2021-02-05 11:29:13 +0100

    Update script.js
    add zad.1-2

---
+++ 9.Konstruktory i prototype/script.js
@@ -0,0 +1,78 @@
+// zad.1
+
+function Pearson(name, surname, age, country, city, language){
+  
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.country = country;
+    this.city = city;
+    this.language = language;
+
+  
+  this.changeAge = function(age) {
+    this.age = age;
+  }
+  
+  this.changeCity = function(city) {
+    this.city = city;
+  }
+}
+const p1 = new Pearson("Paweł", "Dz", 34, "Polska", "Kraków", "Polski");
+const p2 = new Pearson("Paweł", "Kowalski", 12, "Czechy", "Praga", "Czeski");
+const p3 = new Pearson("John", "English", 24, "Anglia", "londyn", "Angielski");
+const p4 = new Pearson("Marek", "Wariat", 55, "Polska", "Warszawa", "Polski");
+const p5 = new Pearson("Carl", "Warrior", 88, "USA", "San Francisco", "Angielski");
+console.log(p1)
+
+
+
+// zad.2
+function add(a, b){
+  return a+b;
+}
+
+function subtract(a, b){
+  return a-b;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function div(a,b){
+  if(b == 0){
+    console.log("Nie można dzielić przez 0");
+    return;
+  }
+  return a/b;
+}
+
+function Calculator(){
+  this.memory = [];
+  this.showMemory = () => console.log(this.memory);
+  this.resetMemory =() => this.memory = [];
+  this.calculate = (a,b,operation) => {
+    this.memory.push(operation(a,b));
+    return operation(a,b);
+  }
+}
+
+let calc1 = new Calculator();
+let calc2 = new Calculator();
+
+console.log(calc1.calculate(4, 4, add));
+console.log(calc1.calculate(6, 2, div));
+console.log(calc1.memory);
+
+console.log(calc2.calculate(11, 4, subtract));
+console.log(calc2.calculate(6, 3, multiply));
+console.log(calc2.memory);
+
+calc1.showMemory();
+calc2.resetMemory();
+console.log(calc2.showMemory());
+
+
+
+// zad.3
