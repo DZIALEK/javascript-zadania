5bca07e16867ab34f6070722a29f937d319432ea
DZIALEK <62839554+DZIALEK@users.noreply.github.com>
2021-02-06 07:18:20 +0100

    Update script.js
    done

--- 11.DOM-wyszukiwanie/script.js
+++ 11.DOM-wyszukiwanie/script.js
@@ -17,3 +17,24 @@ showTags("ul");
 // zad.3
 const secondList = document.getElementById("list");
 console.log(secondList);
+
+
+
+// zad.4
+
+const showElements = (className, idName) => {
+    const liElements = document.getElementsByTagName('li');
+    const ulElements = document.getElementsByTagName('ul');
+    const spanElements = document.getElementsByTagName('span');
+
+    const spanInClass = document.querySelectorAll(`.${className} span`)
+    const spanInId = document.querySelectorAll(`#${idName} span`)
+
+    console.log(liElements)
+    console.log(ulElements)   
+    console.log(spanElements)   
+    console.log(spanInClass)   
+    console.log(spanInId)     
+}
+
+showElements("list", "spans")
