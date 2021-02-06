7761b04542a777e7525da890b3ee307a4e3f89a9
DZIALEK <62839554+DZIALEK@users.noreply.github.com>
2021-02-06 06:53:37 +0100

    Update script.js
    add zad.1-3

---
+++ 11.DOM-wyszukiwanie/script.js
@@ -0,0 +1,19 @@
+// zad.1
+const list = document.getElementsByClassName("list");
+console.log(list);
+
+
+
+// zad.2
+const showTags = (tag) => {
+  const tags = document.getElementsByTagName(tag);
+  console.log(tags);
+};
+showTags("div");
+showTags("ul");
+
+
+
+// zad.3
+const secondList = document.getElementById("list");
+console.log(secondList);
